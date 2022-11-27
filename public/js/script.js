d5f68b925e41b12f8a00544edf28ed8215fae95b
krupanjac <arsen.djurdjev@gmail.com>
2022-11-27 16:39:10 +0100

    quick fix

--- public/js/script.js
+++ public/js/script.js
@@ -13,16 +13,3 @@
     checkbox.checked = false;
   }, false);
 })(document);
-
-
-$(window).scroll(function() {
-  // スクロールの位置取得 
-  // Get scroll position
-  var s = $(window).scrollTop(),
-  // スクロールの値と透明度
-  // scroll value and opacity
-  opacityVal = (s / 150.0);
-  // blurの画像の透明度を0%から100％
-  // opacity value 0% to 100%
-  $('.blurred-img').css('opacity', opacityVal);
-});
